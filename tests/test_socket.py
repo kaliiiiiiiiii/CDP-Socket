a3c93ca7900f90654ccc1d5e562b547161577d91
kaliiiiiiiiii <aurinliun@gmx.ch>
2023-08-11 13:35:33 +0200

    improved tests

--- tests/test_socket.py
+++ tests/test_socket.py
@@ -14,15 +14,17 @@ loop = asyncio.get_event_loop()
 async def test_exec():
     global sock
     res = await sock.exec("Browser.getVersion")
-    return res['product'] + "\n"
+    product = res['product']
+    return f"product: {product}"
 
 
 async def test_wait_for():
     global loop
     await sock.exec("Page.enable")
-    loop.create_task(sock.exec("Page.navigate", {"url": "https://nowsecure.nl#relax"}))
+    loop.create_task(sock.exec("Page.navigate", {"url": "chrome://version"}))
     res = await sock.wait_for("Page.domContentEventFired")
-    return res['timestamp']
+    monotonic_time = res['timestamp']
+    return f"TimeStamp: {monotonic_time}"
 
 
 async def make_socket():
@@ -52,7 +54,7 @@ class Driver(unittest.TestCase):
             test_exec(),
             test_wait_for()
         )
-        print(*res)
+        print("\n".join(res))
         await sock.close()
         os.kill(pid, 15)
 
